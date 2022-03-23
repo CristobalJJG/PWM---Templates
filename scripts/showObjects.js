1339db08119afe5c33d38f1ae8bb17dc18f9fb2b
Cristóbal <cristobal_jjg@hotmail.com>
2022-03-23 08:34:49 +0000

    Cambios en la carga de objetos
    Se han hecho cambios en la cargar de objetos con intención de modularizar (No conseguido). Se han cargado los datos del catalogo de ventas (A falta de terminarlo y/o poner más)
    
    En principio, aquí concluiría el Sprint 2, a falta de hablar con el profesor.

---
+++ scripts/showObjects.js
@@ -0,0 +1,27 @@
+export function showObjects(p) {
+    const div = document.createElement('div');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    const div2 = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+
+    div.setAttribute('class', 'imagen');
+    a.setAttribute('href', "/web/Compra/ComprarObjeto/objEmpresa.html?id=" + p.id);
+    img.setAttribute('src', p.mainPhoto);
+    div2.setAttribute('class', 'content');
+    h2.textContent = p.nombre;
+    p1.textContent = p.descripcion;
+    p2.setAttribute('id', p.id)
+    p2.textContent = p.precio + "€";
+
+    div.appendChild(a);
+    a.appendChild(img);
+    a.appendChild(div2);
+    div2.appendChild(h2);
+    div2.appendChild(p1);
+    div2.appendChild(p2);
+    var sec = document.getElementById('productos');
+    sec.appendChild(div);
+}
